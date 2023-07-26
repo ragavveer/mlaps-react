2ef0cc900f868698cbfe75ce7647f38fb410f0c2
veera sasi <veerasasi@veeras-MacBook-Pro.local>
2023-07-26 16:34:02 +0530

    home screen changes

--- src/routes/static/cover.jsx
+++ src/routes/static/cover.jsx
@@ -1,14 +1,19 @@
-import { Button, Tabs, Tab } from "@mui/material";
+import { Button, Tabs, Tab, Icon } from "@mui/material";
 import { useState } from "react";
-import { Outlet, useLocation, useNavigate, Navigate } from "react-router-dom";
+import { Outlet, useNavigate, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { token, menu } from "../../features/Auth/authSlice";
 
 import "./cover.css";
 
+const heroIcon = (
+  <Icon sx={{ marginTop: "-8px" }}>
+    <img alt="hero" className="hero-icon" />
+  </Icon>
+);
+
 export default function Cover() {
   const navigate = useNavigate();
-  const location = useLocation();
   const accessToken = useSelector(token);
   const protectedMenu = useSelector(menu);
 
@@ -24,19 +29,14 @@ export default function Cover() {
   let naviagationElement;
 
   const isValidUser = () => {
-    let { pathname } = window.location;
-    if (pathname === "/" || pathname === "/home") {
-      if (!accessToken) {
-        navigate("/");
+    if (accessToken) {
+      if (protectedMenu?.length > 0) {
+        const getPath = `/authorized/${protectedMenu[0]}`;
+        // user refresh/enters url with no segments, like "/authorized". So we need to select the default menu
+        naviagationElement = <Navigate to={getPath} />;
       } else {
-        if (protectedMenu?.length > 0) {
-          const getPath = `/authorized/${protectedMenu[0]}`;
-          // user refresh/enters url with no segments, like "/authorized". So we need to select the default menu
-          naviagationElement = <Navigate to={getPath} />;
-        } else {
-          // user refresh/enters url with no segments, like "/authorized". But users dont have menu options.
-          naviagationElement = <Navigate to="/" />;
-        }
+        // user refresh/enters url with no segments, like "/authorized". But users dont have menu options.
+        naviagationElement = <Navigate to="/authorized/default" />;
       }
     }
   };
@@ -53,25 +53,53 @@ export default function Cover() {
       {naviagationElement}
       <header>
         <div className="top-bar"></div>
+        <div className="header">
+          <div
+            className="logo"
+            onClick={() => {
+              setValue(false);
+              navigate("/home");
+            }}
+          ></div>
+          <div className="actions">
+            <Button
+              sx={{
+                textTransform: "none",
+                fontWeight: 200,
+              }}
+              color="inherit"
+              startIcon={heroIcon}
+              onClick={() => {
+                navigate("/authorized/login", { state: "/authorized/" });
+              }}
+            >
+              Sign In
+            </Button>
+          </div>
+          <div className="application-brand">Mobility Labs</div>
+          <div>
+            <Tabs
+              sx={{ minHeight: "2.5rem", height: "2.5rem" }}
+              value={value}
+              onChange={handleChange}
+              aria-label="lab API tabs example"
+            >
+              <Tab
+                sx={{
+                  textTransform: "none",
+                  minHeight: "2.5rem",
+                  height: "2.5rem",
+                }}
+                label="About"
+                value="about"
+              />
+            </Tabs>
+          </div>
+        </div>
       </header>
-      <div>
-        <Button
-          variant="contained"
-          onClick={() => {
-            navigate("/authorized/login", { state: "/authorized/" });
-          }}
-        >
-          Login
-        </Button>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="lab API tabs example"
-        >
-          <Tab label="About" value="about" />
-        </Tabs>
-      </div>
-      <Outlet />
+      <section className="content">
+        <Outlet />
+      </section>
     </>
   );
 }
